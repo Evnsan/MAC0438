67a0ac50bfee6a2973a17807ed6fb4dbd74dbc9d
Evnsan <evnsanches@ig.com.br>
2016-03-14 12:25:26 -0300

    Somente o cabeçalho do ep1

---
+++ EP1/ep1.c
@@ -0,0 +1,29 @@
+/*******************************************************************/
+/**   MAC 438  - Programac~ao Concorrente                         **/
+/**   IME-USP  - Primeiro Semestre de 2016                        **/
+/**   Prof. Marcel Parolin Jackowski                              **/
+/**                                                               **/
+/**   Primeiro Exerccio-Programa                                  **/
+/**   Arquivo: EP1.c                                              **/
+/**                                                               **/
+/**   <nome do(a) aluno(a)>                <numero USP>           **/
+/**   Evandro Augusto Nunes Sanches          5388861              **/
+/**   Fernanda de Camargo Magano             8536044              **/
+/**                                                               **/
+/**   <data de entrega>                                           **/
+/**      08/03/2016                                               **/
+/*******************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    int i;
+
+    printf("Recebidos %d argumentos!\n", argc);
+    for(i = 0; i < argc; i++){
+        printf("Argumento %d => %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
+

f5fd7bff0a521c50117601ee67e61255a66ecc67
Evnsan <evnsanches@ig.com.br>
2016-03-18 17:24:45 -0300

    Processos 3 e 4 prontos - talvez falte organizar o código, mas é basicamente isso.

--- EP1/process3.c
+++ EP1/process3.c
@@ -18,48 +18,48 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <limits.h>
 #include "process3.h"
 
 /*Static valors*/
-#define DEBUG 1
-#define MAXVALUES 25000
-#define MINVALUES 1
+#define DEBUG 0
 #define PIX2 6.28318530718  /*Aproximadamente 2 * PI*/
 
 
 /**process - P3 Buffon's needle**/
-/*int p3(int r){*/
-int main() {
+int p3(int r){
+/*int main() {*/
 	
 	/*t é o número de vezes que a agulha cruza uma linha*/
 	int t, i, j;
-	int r;
 	double result, retorno;
 	double *info[2];
 
-	r = 10;
+	printf("===> P3: Iniciando.\n");
+	printf("===> P3: Será(ão) executado(s) %d lançamento(s).\n",r);
 
+	/***DEBUG***/
+	if (DEBUG){
+		printf("===> P3: Recebido r = %d\n", r);
+    }
+	/***DEBUG***/
 
-	printf("===> P3 começou!\n\n");
-
-	/***/
-	if (DEBUG)
-		printf("   P3 recebeu %d\n", r);
-	/***/
-		
-
-	info[0] = (double *) malloc(r * sizeof(double));
-	info[1] = (double *) malloc(r * sizeof(double));
+    info[0] = (double *) malloc(r * sizeof(double));
+    info[1] = (double *) malloc(r * sizeof(double));
 
 	srand(time(NULL));
 	
-	
-	for (i = 0; i < r; i++) {
-		info[0][i] = ((double)rand() / (double)((unsigned)RAND_MAX + 1));   /*Valor de d*/
-		info[1][i] = ((double)rand() / (double)((unsigned)RAND_MAX + 1)) * PIX2; /*Valor de theta*/
-		
-	}
-
+	/*gerando numeros aleatorios para posicao d e angulo theta de cada
+      lancamento*/
+    for (i = 0; i < r; i++) {
+        info[0][i] = ((double)rand() 
+                      / (double)((unsigned)RAND_MAX + 1)
+                     );   /*Valor de d*/
+        info[1][i] = ((double)rand() 
+                      / (double)((unsigned)RAND_MAX + 1)
+                     ) * PIX2; /*Valor de theta*/
+    }
+    /*contabilizando resultados de interesse (agulha cruza uma linha)*/
 	t = 0;
     for (i = 0; i < r; i++) {
 		result = info[0][i] + sin(info[1][i]);
@@ -68,25 +68,40 @@ int main() {
 			t++;
     }
 
-
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < r; j++) {
-			printf("info[%d][%d] = %lf\n", i, j, info[i][j]);  
-		}
-	}
+	/***DEBUG***/
+	if (DEBUG){
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < r; j++) {
+                printf("info[%d][%d] = %f\n", i, j, info[i][j]);  
+            }
+        }
+    }
+	/***DEBUG***/
 
 	retorno = (double)t/r;
 
-	printf("r = %d t = %d\n", t, r);
-	printf("t\\r = %lf\n", retorno);
+    /**Saida**/
+	printf("===> P3: r = %d, t = %d\n", r, t);
+	printf("===> P3: t/r = %f\n", retorno);
+	printf("===> P3: Probabilidade estimada: %f \n", retorno);
 
+	printf("===> P3: Finalizado.\n");
+	
     
-	
-	printf("===> P3 terminou\n");
-	
-
-
+    free(info[0]);
+    free(info[1]);
+    
     return (int) (retorno * 100);
+}
+
+
+/*para testar*/
+int main(int argc, char** argv){
+    printf("The maximum value of INT = %d\n", INT_MAX);
+    if(argc > 1){
+        p3(atoi(argv[1]));
+    }
+    return 0;
 }
 
 /*******************************/
--- EP1/process4.c
+++ EP1/process4.c
@@ -15,25 +15,93 @@
 /*******************************************************************/
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include "process4.h"
-
+#include <math.h>
+#include <time.h>
+#include <limits.h>
+#include "process3.h"
 
 /*Static valors*/
-#define DEBUG 1
-#define MAXVALUES 25000
-#define MINVALUES 1
+#define DEBUG 0
+#define SEC1 1.85081571768 
 
 
-/******************************************/
-/**process - P4 Integration by simulation**/
-int p4(int s){
-    printf("   P4 recebeu %d\n", s);
-    printf("  Vai terminar com ERRO o processo de pid = %d \n", getpid());
-    exit(1);
-    return 1;
+/**process - P4 Integrate by simulation**/
+double p4(int s){
+	
+	/*s é o número de pontos sorteados*/
+	/*t é o número de pontos no interior da regiao entre a curva (sec(x)) e o
+      eixo x)*/
+	int t, i, j;
+	double retorno;
+	double *info[2];
+
+	printf("===> P4: Iniciando.\n");
+	printf("===> P4: Será(ão) sorteado(s) %d ponto(s).\n",s);
+
+	/***DEBUG***/
+	if (DEBUG){
+		printf("===> P4: Recebido s = %d\n", s);
+    }
+	/***DEBUG***/
+
+    /*info guardara os pontos gerados
+      (ie. info[0][5]= 3 e info[1][5] = 4 <=> p5 = (3,4))
+      obs p5 - sexto ponto gerado (x,y)*/
+
+    info[0] = (double *) malloc(s * sizeof(double));
+    info[1] = (double *) malloc(s * sizeof(double));
+
+	srand(time(NULL));
+	
+	/*gerando numeros aleatoriamente para eixos x e y de cada ponto*/
+    for (i = 0; i < s; i++) {
+        info[0][i] = ((double)rand() 
+                      / (double)((unsigned)RAND_MAX + 1)
+                     );
+        info[1][i] = ((double)rand() 
+                      / (double)((unsigned)RAND_MAX + 1)
+                     ) * SEC1;
+    }
+    /*contabilizando resultados de interesse (agulha cruza uma linha)*/
+	t = 0;
+    for (i = 0; i < s; i++) {
+		if (info[1][i] < 1/cos(info[0][i]))
+			t++;
+    }
+
+	/***DEBUG***/
+	if (DEBUG){
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < s; j++) {
+                printf("info[%d][%d] = %f\n", i, j, info[i][j]);  
+            }
+        }
+    }
+	/***DEBUG***/
+
+	retorno = (double)t/s * SEC1;
+
+    /**Saida**/
+	printf("===> P4: s = %d, t = %d\n", s, t);
+	printf("===> P4: t/s * sec(1) = %f\n", retorno);
+	printf("===> P4: Área estimada: %f \n", retorno);
+
+	printf("===> P4: Finalizado.\n");
+	
+    free(info[0]);
+    free(info[1]);
+
+    return retorno;
 }
 
+
+/*para testar*/
+int main(int argc, char** argv){
+    if(argc > 1){
+        p4(atoi(argv[1]));
+    }
+    return 0;
+}
+
+/*******************************/
